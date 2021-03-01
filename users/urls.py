fbc40e879ad01b4e9ab8e221580e029a4585a37a
mcajongbah <malvinajongbah@gmail.com>
2021-03-01 12:04:56 +0000

    templates refactor

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 app_name = 'users'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name = 'home/homepage.html'), name='home' )
+    path('', TemplateView.as_view(template_name = 'base.html'), name='home' )
 ]
